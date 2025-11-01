287a69a5700f61da32aaf5792ac62d19c208cd57
shubhiaggarwal <shubhi00400@gmail.com>
2025-11-02 00:15:33 +0530

    fact

---
+++ fact.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to find factorial
+long long factorial(int n) {
+    if (n <= 1)  // base case
+        return 1;
+    else
+        return n * factorial(n - 1);  // recursive case
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Factorial is not defined for negative numbers." << endl;
+    else
+        cout << "Factorial of " << n << " = " << factorial(n) << endl;
+
+    return 0;
+}
